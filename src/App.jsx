259f143f7fafcf382caa0abc2177fe9ce9dc52f6
Harry Sharma <harrysharma1066@gmail.com>
2023-06-08 11:46:32 +0100

    Add title

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import { AnimeImage } from './components/AnimeImage';
 import { BreadCrumb } from './components/BreadCrumb';
 import {Nav} from './components/Nav';
+
+const AnimeInfo =()=>{
+  return(
+    <div className="flex flex-col">
+      <h1 className="font-sans text-[25px] font-semibold text-[#3e3c3e]">Naruto Shippuden Filler List</h1>
+    </div>
+  )
+}
+
+
 function App() {
     return(
      <div className="flex flex-col h-screen">
       <Nav/>
       <BreadCrumb/>
-      <AnimeImage/>
+      <div className="flex flex-row">
+        <AnimeImage/>
+        <AnimeInfo/>
+      </div>
+      
      </div>
     )
 }
