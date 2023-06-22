eb0457fe7e8a06dfa2e4a5621f4c07357ab4448d
Harry Sharma <harrysharma1066@gmail.com>
2023-06-22 14:59:23 +0100

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,71 @@ import * as React from 'react';
 import { AnimeTabs } from './components/AnimeTabs';
 import { EpisodeList } from './components/EpisodeList';
 
+const RelatedAnime = () => {
+  return(
+  <>
+    <div className="flex flex-col ml-[250px] mt-5 w-[650px]">
+    <div className="font-sans font-bold text-[#3e3c3e] text-[25px]"> Related Show</div>
+    <div className="text-[15px]">
+       <span className="text-[#2785c1] hover:underline cursor-pointer" onClick={()=>{console.log("Naruto click")}}>
+         Naruto
+       </span>
+      
+       ,
+       {" "}
+       <span className="text-[#2785c1] hover:underline cursor-pointer" onClick={()=>{console.log("Boruto click")}}>
+        Boruto: Naruto Next Generations
+       </span> 
+      
+       , 
+       {" "}
+       <span className="text-[#2785c1] hover:underline cursor-pointer" onClick={()=>{console.log("Boruto (Manga) click")}}>
+         Boruto: Naruto Next Generations (Manga Only)
+       </span>
+      
+       ,
+       {" "}
+       <span className="text-[#2785c1] hover:underline cursor-pointer" onClick={()=>{console.log("Naruto film click")}}>
+          Naruto Films
+       </span>
+     
+    </div>
+    
+    <div className="mt-7">
+    <span className="text-[#2785c1] hover:underline cursor-pointer" onClick={()=>{console.log("Login click")}}>Log in</span> or <span className="text-[#2785c1] hover:underline cursor-pointer" onClick={()=>{console.log("Register click")}}>register</span> to post comments
+    </div>
+
+  </div>
+  
+  </>)
+}
+
+
+const Footer = () => {
+  return(<>
+  <div className="flex flex-row text-[#9b9898] text-[14px] mt-10">
+      <div className="flex-1 cursor-pointer ml-[250px]">
+      <span className="hover:underline">
+        Home
+      </span> 
+      {" "}|{" "}
+      <span className="hover:underline">
+         Privacy Policy
+      </span>
+      {" "}|{" "}
+      <span className="hover:underline">
+      Changelog
+      </span>
+      </div>
+
+      <div className="mr-[580px]">
+      © 2023 www.animefillerlist.com
+      </div>
+  </div>
+  
+  </>)
+}
+
 function App() {
     return(
      <div className="flex flex-col h-screen w-screen">
@@ -17,7 +82,8 @@ function App() {
       </div>
       <AnimeTabs/>
       <EpisodeList/>
-
+      <RelatedAnime/>
+      <Footer/>
      </div>
     )
 }
